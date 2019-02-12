40a3befba8eb8cb1d44667d84c16d3305ceaad6f
david mulligan <s00155368@mail.itsligo.ie>
2019-02-12 19:19:49 +0000

    building more of file structure for application

--- lib/backend/miscella.dart
+++ lib/backend/miscella.dart
@@ -1,3 +1,6 @@
+import 'package:encrypt/encrypt.dart';
+import './secrets.dart';
+
 String newId() {
 
   return "hello there,..";
@@ -6,4 +9,16 @@ String newId() {
 String newTimestamp() {
 
   return "hello there,..";
+}
+
+String encrypt(String content) {
+  final encrypter = Encrypter( AES(encryptionKey) );
+
+  return encrypter.encrypt(content);
+}
+
+String decrypt(String cipher) {
+  final encrypter = Encrypter( AES(encryptionKey) );
+
+  return encrypter.decrypt(cipher);
 }
