5a83fcb8262cb3e1314d6ef7ab114c9d72d25bf7
DarranMullen <darran.mullen@mail.itsligo.ie>
2019-02-13 16:07:26 +0000

    view and edit dialogs added

--- lib/inventory/inventory_page.dart
+++ lib/inventory/inventory_page.dart
@@ -1,17 +1,9 @@
 import 'package:flutter/material.dart';
 
 import '../app/dataStore.dart';
-import '../app/logic.dart';
-import '../backend/transactionLog.dart';
+// import '../app/logic.dart';
+// import '../backend/transactionLog.dart';
 import '../models/consumable.dart';
-// var i1 = new Consumable(productId: "1", name: 'Apple', quantity: 2);
-// var i2 = new Consumable(productId: "2", name: 'Banana', quantity: 3);
-// var i3 = new Consumable(productId: "3", name: 'Carrot', quantity: 5);
-// var i4 = new Consumable(productId: "4", name: 'Date', quantity: 7);
-// var i5 = new Consumable(productId: "5", name: 'Grapes', quantity: 11);
-// var i6 = new Consumable(productId: "6", name: 'Lemon', quantity: 13);
-
-// List<Consumable> consumableList = [];
 
 class InventoryPage extends StatefulWidget {
   @override
@@ -22,9 +14,9 @@ class InventoryList extends State<InventoryPage> {
   String reason;
 
   @override
-  Widget build(BuildContext ctxt) {
+  Widget build(BuildContext context) {
     fillList();
-    // consumableList = consumables;
+
     return new Scaffold(
       floatingActionButton: new FloatingActionButton(
         onPressed: () {/* OPEN MODAL */},
@@ -40,51 +32,17 @@ class InventoryList extends State<InventoryPage> {
           return Dismissible(
             key: Key(index.toString()),
             secondaryBackground: Container(color: Colors.red),
-            onDismissed: (direction) {
-              //FYI DAVID!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-              //HERE LIES THE PROBLEM
-              setState(() {
-                // consumables.removeAt(index);
-                if (direction == DismissDirection.startToEnd) {
-                  Consumables.consumed(index);
-                } else {
-                  Consumables.wasted(index);
-                }
-              });
-              // print('consumableList');
-              // for (var item in consumableList) {
-              //   print(item.name);
-              // }
-              print(
-                  '-----------------------------------------------------------');
-
-              print('consumables');
-              for (var item in consumables) {
-                print(item.name);
-              }
-
-              // print("TRANSACTION LOG");
-              // for (var item in transactionLog) {
-              //   print(item.jsonify());
-              // }
-
-              // reason =
-              //     direction == DismissDirection.startToEnd ? "used" : "wasted";
-
-              // Scaffold.of(context).showSnackBar(
-              //     SnackBar(content: Text(consumable.name + ' ' + reason)));
-              // print(consumable.name + ' ' + reason);
-            },
+            onDismissed: (direction) {},
             background: Container(color: Colors.green),
             child: ListTile(
               title: Text(consumable.name),
               trailing: Text(consumable.quantity.toString()),
               onTap: () {
-                // editDialog(context, item);
+                viewDialog(context, consumable);
               },
               onLongPress: () {
-                print(consumable.name + 's Quick Edit');
+                // quickEditDialog(context);
+                // print(consumable.name + 's Quick Edit');
               },
             ),
           );
@@ -92,4 +50,140 @@ class InventoryList extends State<InventoryPage> {
       ),
     );
   }
+}
+
+Future<bool> viewDialog(BuildContext context, Consumable item) {
+  return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Theme(
+          data: Theme.of(context)
+              .copyWith(dialogBackgroundColor: Colors.transparent),
+          child: SimpleDialog(
+            children: <Widget>[
+              Card(
+                elevation: 4,
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Text(
+                        item.name.toUpperCase(),
+                        style: TextStyle(fontSize: 40.0),
+                      ),
+                    ),
+                    Divider(),
+                    Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: Text('PRODUCT ID:'),
+                            flex: 1,
+                          ),
+                          Expanded(
+                            child: Text(
+                              item.productId,
+                            ),
+                            flex: 1,
+                          )
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Row(children: <Widget>[
+                        Expanded(child: Text('QUANTITY: '), flex: 1),
+                        Expanded(
+                            child: Text(item.quantity.toString()), flex: 1),
+                      ]),
+                    ),
+                    Row(
+                      children: <Widget>[
+                        CloseButton(),
+                        FlatButton.icon(
+                          icon: Icon(Icons.edit),
+                          label: Text('EDIT'),
+                          onPressed: () {
+                            editDialog(context, item);
+                          },
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      });
+}
+
+Future<bool> editDialog(BuildContext context, Consumable item) {
+  return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Theme(
+          data: Theme.of(context)
+              .copyWith(dialogBackgroundColor: Colors.transparent),
+          child: SimpleDialog(
+            children: <Widget>[
+              Card(
+                elevation: 4,
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: TextFormField(
+                        initialValue: item.name.toUpperCase(),
+                        style: TextStyle(fontSize: 40.0, color: Colors.red),
+                      ),
+                    ),
+                    Divider(),
+                    Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: Text('PRODUCT ID:'),
+                            flex: 1,
+                          ),
+                          Expanded(
+                            child: TextFormField(
+                              initialValue: item.productId,
+                              enabled: false,
+                            ),
+                            flex: 1,
+                          )
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Row(children: <Widget>[
+                        Expanded(child: Text('QUANTITY: '), flex: 1),
+                        Expanded(
+                            child: TextFormField(
+                              initialValue: item.quantity.toString(),
+                            ),
+                            flex: 1),
+                      ]),
+                    ),
+                    Row(
+                      children: <Widget>[
+                        BackButton(),
+                        FlatButton.icon(
+                          icon: Icon(Icons.save),
+                          label: Text('SAVE'),
+                          onPressed: () {},
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      });
 }
