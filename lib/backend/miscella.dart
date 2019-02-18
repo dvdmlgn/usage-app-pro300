09bc287212687096946c38bc97f7236a95e6c444
david mulligan <s00155368@mail.itsligo.ie>
2019-02-18 15:07:40 +0000

    uncommentd out debug prints

--- lib/backend/miscella.dart
+++ lib/backend/miscella.dart
@@ -21,18 +21,18 @@ String encrypt(String content) {
   int pad = (remainder == 0) ? 0 : 64 - remainder;
   int rounds = (length / 64).ceil();
 
-  debugPrint("rounds: ${rounds}");
+  // debugPrint("rounds: ${rounds}");
 
-  debugPrint('content: ${content}');
+  // debugPrint('content: ${content}');
 
-  debugPrint('length: ${length}');
-  debugPrint('remainder: ${remainder}');
-  debugPrint('pad: ${pad}');
+  // debugPrint('length: ${length}');
+  // debugPrint('remainder: ${remainder}');
+  // debugPrint('pad: ${pad}');
 
   content = content.padRight(64 * rounds, ' ');
 
-  debugPrint('content padded: ${content}');
-  debugPrint('padding lenght: ${content.length}');
+  // debugPrint('content padded: ${content}');
+  // debugPrint('padding lenght: ${content.length}');
 
   String cipher = '';
   int count = 0;
@@ -40,9 +40,9 @@ String encrypt(String content) {
   while(true) {
     String slice = content.substring(count, count + 64);
 
-    debugPrint("slice: ${slice}");
+    // debugPrint("slice: ${slice}");
 
-    debugPrint("slice length: ${slice.length}");
+    // debugPrint("slice length: ${slice.length}");
 
     cipher += encrypter.encrypt(slice);
 
@@ -50,7 +50,7 @@ String encrypt(String content) {
 
     count += 64;
 
-    debugPrint("count: ${count}");
+    // debugPrint("count: ${count}");
 
     if (count >= length) { break; }
   }
@@ -63,7 +63,7 @@ String decrypt(String cipher) {
 
   int length = cipher.length;
 
-  debugPrint("cipher length: ${cipher.length}");
+  // debugPrint("cipher length: ${cipher.length}");
 
   String contents = '';
   int count = 0;
@@ -77,11 +77,11 @@ String decrypt(String cipher) {
     if (count >= length) { break; }
   }
 
-  debugPrint("contents length: ${contents.length}");
+  // debugPrint("contents length: ${contents.length}");
 
   contents = contents.trimRight();
 
-  debugPrint("contents length: ${contents.length}");
+  // debugPrint("contents length: ${contents.length}");
 
   return contents;
 }
