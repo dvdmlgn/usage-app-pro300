acf8b30bef44e23e8fbc36e33fa915317c44fddf
DarranMullen <darran.mullen@mail.itsligo.ie>
2019-02-20 16:01:59 +0000

    shopping list and basket started

--- lib/darranScratchPad.dart
+++ lib/darranScratchPad.dart
@@ -6,6 +6,7 @@ import 'app/logic.dart';
 import 'app/dataStore.dart';
 import './inventory/inventory_page.dart';
 import './inventory/inventory.dart';
+import 'package:usage/shoppingList/shopping.dart';
 // import './inventory/gallery.dart';
 import './backend/auth.dart';
 
@@ -125,13 +126,13 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+      // appBar: AppBar(
+      //   // Here we take the value from the MyHomePage object that was created by
+      //   // the App.build method, and use it to set our appbar title.
+      //   title: Text(widget.title),
+      // ),
       // body: Inventory()
-      body: Inventory(),
+      body: Shopping(),
       // Center(
       //   // Center is a layout widget. It takes a single child and positions it
       //   // in the middle of the parent.
