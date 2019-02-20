223610ef235de3d892c9c8f2e58484965bcbeb3d
Gary <garymccormack@outlook.com>
2019-02-20 11:43:44 +0000

    Create new post nearly working

--- lib/app/dataStore.dart
+++ lib/app/dataStore.dart
@@ -23,77 +23,77 @@ var p1 = new Post
     tags: ["Tag", "Tag", "Tag"]
   );
 
-var p2 = new Post
-  (
-    author: "Paul",
-    title: "This is a title",
-    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id magna tortor. Donec tincidunt neque mi, quis dignissim dolor cursus et. Etiam a arcu sed nisl tincidunt aliquam a nec enim. Phasellus mattis ante quis ligula lobortis, accumsan rhoncus urna elementum. Sed dictum nec lacus quis hendrerit. Quisque ut est elementum, dapibus lacus sit amet, volutpat erat. Proin augue mauris, efficitur vel mattis finibus, iaculis at purus. Quisque felis urna, elementum ac lorem vitae, rutrum lacinia urna. Pellentesque a erat id sapien imperdiet pretium. Ut accumsan augue vitae turpis sodales aliquam. Morbi id ante ligula. Nunc bibendum finibus mi, eu rutrum turpis rutrum et. Sed eu posuere eros. Praesent et viverra enim, vel molestie orci. Nulla vitae risus at risus pellentesque fringilla vel egestas mi.",
-    tags: ["Tag", "Tag", "Tag"]
-);
-
-var p3 = new Post
-  (
-    author: "Paul",
-    title: "This is a title",
-    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id magna tortor. Donec tincidunt neque mi, quis dignissim dolor cursus et. Etiam a arcu sed nisl tincidunt aliquam a nec enim. Phasellus mattis ante quis ligula lobortis, accumsan rhoncus urna elementum. Sed dictum nec lacus quis hendrerit. Quisque ut est elementum, dapibus lacus sit amet, volutpat erat. Proin augue mauris, efficitur vel mattis finibus, iaculis at purus. Quisque felis urna, elementum ac lorem vitae, rutrum lacinia urna. Pellentesque a erat id sapien imperdiet pretium. Ut accumsan augue vitae turpis sodales aliquam. Morbi id ante ligula. Nunc bibendum finibus mi, eu rutrum turpis rutrum et. Sed eu posuere eros. Praesent et viverra enim, vel molestie orci. Nulla vitae risus at risus pellentesque fringilla vel egestas mi.",
-    tags: ["Tag", "Tag", "Tag"]
-);
-
-var p4 = new Post
-  (
-    author: "Paul",
-    title: "This is a title",
-    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id magna tortor. Donec tincidunt neque mi, quis dignissim dolor cursus et. Etiam a arcu sed nisl tincidunt aliquam a nec enim. Phasellus mattis ante quis ligula lobortis, accumsan rhoncus urna elementum. Sed dictum nec lacus quis hendrerit. Quisque ut est elementum, dapibus lacus sit amet, volutpat erat. Proin augue mauris, efficitur vel mattis finibus, iaculis at purus. Quisque felis urna, elementum ac lorem vitae, rutrum lacinia urna. Pellentesque a erat id sapien imperdiet pretium. Ut accumsan augue vitae turpis sodales aliquam. Morbi id ante ligula. Nunc bibendum finibus mi, eu rutrum turpis rutrum et. Sed eu posuere eros. Praesent et viverra enim, vel molestie orci. Nulla vitae risus at risus pellentesque fringilla vel egestas mi.",
-    tags: ["Tag", "Tag", "Tag"]
-);
-
-var p5 = new Post
-  (
-    author: "Paul",
-    title: "This is a title",
-    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id magna tortor. Donec tincidunt neque mi, quis dignissim dolor cursus et. Etiam a arcu sed nisl tincidunt aliquam a nec enim. Phasellus mattis ante quis ligula lobortis, accumsan rhoncus urna elementum. Sed dictum nec lacus quis hendrerit. Quisque ut est elementum, dapibus lacus sit amet, volutpat erat. Proin augue mauris, efficitur vel mattis finibus, iaculis at purus. Quisque felis urna, elementum ac lorem vitae, rutrum lacinia urna. Pellentesque a erat id sapien imperdiet pretium. Ut accumsan augue vitae turpis sodales aliquam. Morbi id ante ligula. Nunc bibendum finibus mi, eu rutrum turpis rutrum et. Sed eu posuere eros. Praesent et viverra enim, vel molestie orci. Nulla vitae risus at risus pellentesque fringilla vel egestas mi.",
-    tags: ["Tag", "Tag", "Tag"]
-);
-
-var p6 = new Post
-  (
-    author: "Paul",
-    title: "This is a title",
-    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id magna tortor. Donec tincidunt neque mi, quis dignissim dolor cursus et. Etiam a arcu sed nisl tincidunt aliquam a nec enim. Phasellus mattis ante quis ligula lobortis, accumsan rhoncus urna elementum. Sed dictum nec lacus quis hendrerit. Quisque ut est elementum, dapibus lacus sit amet, volutpat erat. Proin augue mauris, efficitur vel mattis finibus, iaculis at purus. Quisque felis urna, elementum ac lorem vitae, rutrum lacinia urna. Pellentesque a erat id sapien imperdiet pretium. Ut accumsan augue vitae turpis sodales aliquam. Morbi id ante ligula. Nunc bibendum finibus mi, eu rutrum turpis rutrum et. Sed eu posuere eros. Praesent et viverra enim, vel molestie orci. Nulla vitae risus at risus pellentesque fringilla vel egestas mi.",
-    tags: ["Tag", "Tag", "Tag"]
-);
-
-var p7 = new Post
-  (
-    author: "Paul",
-    title: "This is a title",
-    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id magna tortor. Donec tincidunt neque mi, quis dignissim dolor cursus et. Etiam a arcu sed nisl tincidunt aliquam a nec enim. Phasellus mattis ante quis ligula lobortis, accumsan rhoncus urna elementum. Sed dictum nec lacus quis hendrerit. Quisque ut est elementum, dapibus lacus sit amet, volutpat erat. Proin augue mauris, efficitur vel mattis finibus, iaculis at purus. Quisque felis urna, elementum ac lorem vitae, rutrum lacinia urna. Pellentesque a erat id sapien imperdiet pretium. Ut accumsan augue vitae turpis sodales aliquam. Morbi id ante ligula. Nunc bibendum finibus mi, eu rutrum turpis rutrum et. Sed eu posuere eros. Praesent et viverra enim, vel molestie orci. Nulla vitae risus at risus pellentesque fringilla vel egestas mi.",
-    tags: ["Tag", "Tag", "Tag"]
-);
-
-var p8 = new Post
-  (
-    author: "Paul",
-    title: "This is a title",
-    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id magna tortor. Donec tincidunt neque mi, quis dignissim dolor cursus et. Etiam a arcu sed nisl tincidunt aliquam a nec enim. Phasellus mattis ante quis ligula lobortis, accumsan rhoncus urna elementum. Sed dictum nec lacus quis hendrerit. Quisque ut est elementum, dapibus lacus sit amet, volutpat erat. Proin augue mauris, efficitur vel mattis finibus, iaculis at purus. Quisque felis urna, elementum ac lorem vitae, rutrum lacinia urna. Pellentesque a erat id sapien imperdiet pretium. Ut accumsan augue vitae turpis sodales aliquam. Morbi id ante ligula. Nunc bibendum finibus mi, eu rutrum turpis rutrum et. Sed eu posuere eros. Praesent et viverra enim, vel molestie orci. Nulla vitae risus at risus pellentesque fringilla vel egestas mi.",
-    tags: ["Tag", "Tag", "Tag"]
-);
-
-var p9 = new Post
-  (
-    author: "Paul",
-    title: "This is a title",
-    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id magna tortor. Donec tincidunt neque mi, quis dignissim dolor cursus et. Etiam a arcu sed nisl tincidunt aliquam a nec enim. Phasellus mattis ante quis ligula lobortis, accumsan rhoncus urna elementum. Sed dictum nec lacus quis hendrerit. Quisque ut est elementum, dapibus lacus sit amet, volutpat erat. Proin augue mauris, efficitur vel mattis finibus, iaculis at purus. Quisque felis urna, elementum ac lorem vitae, rutrum lacinia urna. Pellentesque a erat id sapien imperdiet pretium. Ut accumsan augue vitae turpis sodales aliquam. Morbi id ante ligula. Nunc bibendum finibus mi, eu rutrum turpis rutrum et. Sed eu posuere eros. Praesent et viverra enim, vel molestie orci. Nulla vitae risus at risus pellentesque fringilla vel egestas mi.",
-    tags: ["Tag", "Tag", "Tag"]
-);
-
-var p10 = new Post
-  (
-    author: "Paul",
-    title: "This is a title",
-    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id magna tortor. Donec tincidunt neque mi, quis dignissim dolor cursus et. Etiam a arcu sed nisl tincidunt aliquam a nec enim. Phasellus mattis ante quis ligula lobortis, accumsan rhoncus urna elementum. Sed dictum nec lacus quis hendrerit. Quisque ut est elementum, dapibus lacus sit amet, volutpat erat. Proin augue mauris, efficitur vel mattis finibus, iaculis at purus. Quisque felis urna, elementum ac lorem vitae, rutrum lacinia urna. Pellentesque a erat id sapien imperdiet pretium. Ut accumsan augue vitae turpis sodales aliquam. Morbi id ante ligula. Nunc bibendum finibus mi, eu rutrum turpis rutrum et. Sed eu posuere eros. Praesent et viverra enim, vel molestie orci. Nulla vitae risus at risus pellentesque fringilla vel egestas mi.",
-    tags: ["Tag", "Tag", "Tag"]
-);
+//var p2 = new Post
+//  (
+//    author: "Paul",
+//    title: "This is a title",
+//    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id magna tortor. Donec tincidunt neque mi, quis dignissim dolor cursus et. Etiam a arcu sed nisl tincidunt aliquam a nec enim. Phasellus mattis ante quis ligula lobortis, accumsan rhoncus urna elementum. Sed dictum nec lacus quis hendrerit. Quisque ut est elementum, dapibus lacus sit amet, volutpat erat. Proin augue mauris, efficitur vel mattis finibus, iaculis at purus. Quisque felis urna, elementum ac lorem vitae, rutrum lacinia urna. Pellentesque a erat id sapien imperdiet pretium. Ut accumsan augue vitae turpis sodales aliquam. Morbi id ante ligula. Nunc bibendum finibus mi, eu rutrum turpis rutrum et. Sed eu posuere eros. Praesent et viverra enim, vel molestie orci. Nulla vitae risus at risus pellentesque fringilla vel egestas mi.",
+//    tags: ["Tag", "Tag", "Tag"]
+//);
+//
+//var p3 = new Post
+//  (
+//    author: "Paul",
+//    title: "This is a title",
+//    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id magna tortor. Donec tincidunt neque mi, quis dignissim dolor cursus et. Etiam a arcu sed nisl tincidunt aliquam a nec enim. Phasellus mattis ante quis ligula lobortis, accumsan rhoncus urna elementum. Sed dictum nec lacus quis hendrerit. Quisque ut est elementum, dapibus lacus sit amet, volutpat erat. Proin augue mauris, efficitur vel mattis finibus, iaculis at purus. Quisque felis urna, elementum ac lorem vitae, rutrum lacinia urna. Pellentesque a erat id sapien imperdiet pretium. Ut accumsan augue vitae turpis sodales aliquam. Morbi id ante ligula. Nunc bibendum finibus mi, eu rutrum turpis rutrum et. Sed eu posuere eros. Praesent et viverra enim, vel molestie orci. Nulla vitae risus at risus pellentesque fringilla vel egestas mi.",
+//    tags: ["Tag", "Tag", "Tag"]
+//);
+//
+//var p4 = new Post
+//  (
+//    author: "Paul",
+//    title: "This is a title",
+//    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id magna tortor. Donec tincidunt neque mi, quis dignissim dolor cursus et. Etiam a arcu sed nisl tincidunt aliquam a nec enim. Phasellus mattis ante quis ligula lobortis, accumsan rhoncus urna elementum. Sed dictum nec lacus quis hendrerit. Quisque ut est elementum, dapibus lacus sit amet, volutpat erat. Proin augue mauris, efficitur vel mattis finibus, iaculis at purus. Quisque felis urna, elementum ac lorem vitae, rutrum lacinia urna. Pellentesque a erat id sapien imperdiet pretium. Ut accumsan augue vitae turpis sodales aliquam. Morbi id ante ligula. Nunc bibendum finibus mi, eu rutrum turpis rutrum et. Sed eu posuere eros. Praesent et viverra enim, vel molestie orci. Nulla vitae risus at risus pellentesque fringilla vel egestas mi.",
+//    tags: ["Tag", "Tag", "Tag"]
+//);
+//
+//var p5 = new Post
+//  (
+//    author: "Paul",
+//    title: "This is a title",
+//    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id magna tortor. Donec tincidunt neque mi, quis dignissim dolor cursus et. Etiam a arcu sed nisl tincidunt aliquam a nec enim. Phasellus mattis ante quis ligula lobortis, accumsan rhoncus urna elementum. Sed dictum nec lacus quis hendrerit. Quisque ut est elementum, dapibus lacus sit amet, volutpat erat. Proin augue mauris, efficitur vel mattis finibus, iaculis at purus. Quisque felis urna, elementum ac lorem vitae, rutrum lacinia urna. Pellentesque a erat id sapien imperdiet pretium. Ut accumsan augue vitae turpis sodales aliquam. Morbi id ante ligula. Nunc bibendum finibus mi, eu rutrum turpis rutrum et. Sed eu posuere eros. Praesent et viverra enim, vel molestie orci. Nulla vitae risus at risus pellentesque fringilla vel egestas mi.",
+//    tags: ["Tag", "Tag", "Tag"]
+//);
+//
+//var p6 = new Post
+//  (
+//    author: "Paul",
+//    title: "This is a title",
+//    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id magna tortor. Donec tincidunt neque mi, quis dignissim dolor cursus et. Etiam a arcu sed nisl tincidunt aliquam a nec enim. Phasellus mattis ante quis ligula lobortis, accumsan rhoncus urna elementum. Sed dictum nec lacus quis hendrerit. Quisque ut est elementum, dapibus lacus sit amet, volutpat erat. Proin augue mauris, efficitur vel mattis finibus, iaculis at purus. Quisque felis urna, elementum ac lorem vitae, rutrum lacinia urna. Pellentesque a erat id sapien imperdiet pretium. Ut accumsan augue vitae turpis sodales aliquam. Morbi id ante ligula. Nunc bibendum finibus mi, eu rutrum turpis rutrum et. Sed eu posuere eros. Praesent et viverra enim, vel molestie orci. Nulla vitae risus at risus pellentesque fringilla vel egestas mi.",
+//    tags: ["Tag", "Tag", "Tag"]
+//);
+//
+//var p7 = new Post
+//  (
+//    author: "Paul",
+//    title: "This is a title",
+//    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id magna tortor. Donec tincidunt neque mi, quis dignissim dolor cursus et. Etiam a arcu sed nisl tincidunt aliquam a nec enim. Phasellus mattis ante quis ligula lobortis, accumsan rhoncus urna elementum. Sed dictum nec lacus quis hendrerit. Quisque ut est elementum, dapibus lacus sit amet, volutpat erat. Proin augue mauris, efficitur vel mattis finibus, iaculis at purus. Quisque felis urna, elementum ac lorem vitae, rutrum lacinia urna. Pellentesque a erat id sapien imperdiet pretium. Ut accumsan augue vitae turpis sodales aliquam. Morbi id ante ligula. Nunc bibendum finibus mi, eu rutrum turpis rutrum et. Sed eu posuere eros. Praesent et viverra enim, vel molestie orci. Nulla vitae risus at risus pellentesque fringilla vel egestas mi.",
+//    tags: ["Tag", "Tag", "Tag"]
+//);
+//
+//var p8 = new Post
+//  (
+//    author: "Paul",
+//    title: "This is a title",
+//    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id magna tortor. Donec tincidunt neque mi, quis dignissim dolor cursus et. Etiam a arcu sed nisl tincidunt aliquam a nec enim. Phasellus mattis ante quis ligula lobortis, accumsan rhoncus urna elementum. Sed dictum nec lacus quis hendrerit. Quisque ut est elementum, dapibus lacus sit amet, volutpat erat. Proin augue mauris, efficitur vel mattis finibus, iaculis at purus. Quisque felis urna, elementum ac lorem vitae, rutrum lacinia urna. Pellentesque a erat id sapien imperdiet pretium. Ut accumsan augue vitae turpis sodales aliquam. Morbi id ante ligula. Nunc bibendum finibus mi, eu rutrum turpis rutrum et. Sed eu posuere eros. Praesent et viverra enim, vel molestie orci. Nulla vitae risus at risus pellentesque fringilla vel egestas mi.",
+//    tags: ["Tag", "Tag", "Tag"]
+//);
+//
+//var p9 = new Post
+//  (
+//    author: "Paul",
+//    title: "This is a title",
+//    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id magna tortor. Donec tincidunt neque mi, quis dignissim dolor cursus et. Etiam a arcu sed nisl tincidunt aliquam a nec enim. Phasellus mattis ante quis ligula lobortis, accumsan rhoncus urna elementum. Sed dictum nec lacus quis hendrerit. Quisque ut est elementum, dapibus lacus sit amet, volutpat erat. Proin augue mauris, efficitur vel mattis finibus, iaculis at purus. Quisque felis urna, elementum ac lorem vitae, rutrum lacinia urna. Pellentesque a erat id sapien imperdiet pretium. Ut accumsan augue vitae turpis sodales aliquam. Morbi id ante ligula. Nunc bibendum finibus mi, eu rutrum turpis rutrum et. Sed eu posuere eros. Praesent et viverra enim, vel molestie orci. Nulla vitae risus at risus pellentesque fringilla vel egestas mi.",
+//    tags: ["Tag", "Tag", "Tag"]
+//);
+//
+//var p10 = new Post
+//  (
+//    author: "Paul",
+//    title: "This is a title",
+//    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id magna tortor. Donec tincidunt neque mi, quis dignissim dolor cursus et. Etiam a arcu sed nisl tincidunt aliquam a nec enim. Phasellus mattis ante quis ligula lobortis, accumsan rhoncus urna elementum. Sed dictum nec lacus quis hendrerit. Quisque ut est elementum, dapibus lacus sit amet, volutpat erat. Proin augue mauris, efficitur vel mattis finibus, iaculis at purus. Quisque felis urna, elementum ac lorem vitae, rutrum lacinia urna. Pellentesque a erat id sapien imperdiet pretium. Ut accumsan augue vitae turpis sodales aliquam. Morbi id ante ligula. Nunc bibendum finibus mi, eu rutrum turpis rutrum et. Sed eu posuere eros. Praesent et viverra enim, vel molestie orci. Nulla vitae risus at risus pellentesque fringilla vel egestas mi.",
+//    tags: ["Tag", "Tag", "Tag"]
+//);
 
 void fillList() {
   consumables.add(i1);
@@ -108,15 +108,15 @@ List<Post> fillPosts(){
   List<Post> samplePosts = new List<Post>();
 
   samplePosts.add(p1);
-  samplePosts.add(p2);
-  samplePosts.add(p3);
-  samplePosts.add(p4);
-  samplePosts.add(p5);
-  samplePosts.add(p6);
-  samplePosts.add(p7);
-  samplePosts.add(p8);
-  samplePosts.add(p9);
-  samplePosts.add(p10);
+//  samplePosts.add(p2);
+//  samplePosts.add(p3);
+//  samplePosts.add(p4);
+//  samplePosts.add(p5);
+//  samplePosts.add(p6);
+//  samplePosts.add(p7);
+//  samplePosts.add(p8);
+//  samplePosts.add(p9);
+//  samplePosts.add(p10);
 
   return samplePosts;
 }
--- lib/social/social.dart
+++ lib/social/social.dart
@@ -9,85 +9,152 @@ class Social extends StatefulWidget {
 
 class _SocialState extends State<Social> {
   List<Post> myPosts = posts;
+  final TextEditingController _titleController =
+  new TextEditingController();
+  final TextEditingController _contentController =
+  new TextEditingController();
+  String title;
+  String content;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        floatingActionButton: new FloatingActionButton(
-            tooltip: "Add Item",
-            backgroundColor: Colors.blue,
-            child: new ListTile(
-              title: Icon(Icons.add),
-            ),
-            onPressed: _addPost),
-        appBar: AppBar(
-          title: Text("Social"),
-          actions: <Widget>[
-            new IconButton(
-                icon: Icon(
-                  Icons.notifications,
-                  color: Colors.white,
-                ),
-                onPressed: () {}),
-          ],
-        ),
-        body: Column(
-          children: <Widget>[
-            Flexible(
-              child: new ListView.builder(
-                padding: new EdgeInsets.all(8.0),
-                reverse: false,
-                itemCount: myPosts.length,
-                itemBuilder: (_, int index) {
-                  return Card(
-                    child: Column(
-                      children: <Widget>[
-                        ListTile(
-                          title: Text(
-                            myPosts[index].title,
-                            style: TextStyle(fontSize: 24.0),
-                          ),
-                          subtitle: Text(
-                                myPosts[index].content,
-                                textAlign: TextAlign.justify,
-                                overflow: TextOverflow.ellipsis,
-                                style: TextStyle(
-                                    fontSize: 16.0,
-                                    fontWeight: FontWeight.bold),
-                                maxLines: 10,
-                              ),
-                          trailing: IconButton(
-                              icon: Icon(
-                                Icons.add,
-                                color: Colors.black,
-                              ),
-                              onPressed: () {}),
-                        )
-                      ],
-                    ),
-                  );
-                },
+      floatingActionButton: new FloatingActionButton(
+          tooltip: "Add Item",
+          backgroundColor: Colors.blue,
+          child: new ListTile(
+            title: Icon(Icons.add),
+          ),
+          onPressed: _addPost),
+      appBar: AppBar(
+        title: Text("Social"),
+        actions: <Widget>[
+          new IconButton(
+              icon: Icon(
+                Icons.notifications,
+                color: Colors.white,
               ),
-            )
-          ],
-        ),
+              onPressed: () {}),
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
+          Flexible(
+            child: new ListView.builder(
+              padding: new EdgeInsets.all(8.0),
+              reverse: false,
+              itemCount: myPosts.length,
+              itemBuilder: (_, int index) {
+                return Card(
+                  child: Column(
+                    children: <Widget>[
+                      ListTile(
+                        title: Text(
+                          myPosts[index].title,
+                          style: TextStyle(fontSize: 24.0),
+                        ),
+                        subtitle: Text(
+                          myPosts[index].content,
+                          textAlign: TextAlign.justify,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                              fontSize: 16.0, fontWeight: FontWeight.bold),
+                          maxLines: 10,
+                        ),
+                        trailing: IconButton(
+                            icon: Icon(
+                              Icons.add,
+                              color: Colors.black,
+                            ),
+                            onPressed: () {}
+                        ),
+                        onTap: () {
+                          Navigator.push(context, MaterialPageRoute(
+                              builder: (BuildContext context) {
+                                return HeroPage();
+                              }));
+                        },
+                      )
+                    ],
+                  ),
+                );
+              },
+            ),
+          )
+        ],
+      ),
     );
   }
 
-  Post _addPost() {
-    Post newPost;
+
+  void _addPost() {
+    var alert = new AlertDialog(
+      content: Column(
+        children: <Widget>[
+          new Row(
+            children: <Widget>[
+              new Expanded(
+                  child: new TextField(
+                    controller: _titleController,
+                    autofocus: true,
+                    decoration: new InputDecoration(
+                        labelText: "Title",
+                        hintText: "Enter a title for your post"),
+                  ))
+            ],
+          ),
+          new Row(
+            children: <Widget>[
+              new Expanded(
+                  child: new TextField(
+                    controller: _contentController,
+                    autofocus: true,
+                    decoration: new InputDecoration(
+                        labelText: "Post content",
+                        hintText: "What's on your mind?"),
+                  ))
+            ],
+          )
+        ],
+      ),
+      actions: <Widget>[
+        new FlatButton(
+            onPressed: () {
+              title = _titleController.toString();
+              content = _contentController.toString();
+
+              Post newPost = new Post(title: title, content: content);
+
+              setState(() {
+                myPosts.add(newPost);
+              });
+
+              Navigator.pop(context);
+            },
+            child: Text("Save")),
+        new FlatButton(
+            onPressed: () => Navigator.pop(context), child: Text("Cancel"))
+      ],
+    );
     showDialog(
         context: context,
-        builder: (_) => new AlertDialog(
-            title: new Text("New Post"),
-            content: Column(
-              children: <Widget>[
-                Text("Title"),
-                TextField(),
-                Text("Content"),
-                TextField()
-              ],
-            )));
-    return newPost;
+        builder: (_) {
+          return alert;
+        });
+  }
+}
+
+class HeroPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: <Widget>[
+          Text("Title"),
+          Image.network('https://imgplaceholder.com/420x320/ff7f7f/333333/fa-image', fit: BoxFit.fitWidth)
+        ],
+      )
+    );
   }
 }
