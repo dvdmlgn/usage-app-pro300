35db138e6259cae1e6bbeaa9fdb3084ec8f9ce16
david mulligan <s00155368@mail.itsligo.ie>
2019-03-11 14:21:03 +0000

    created function definitions for populating lists with dummy data

--- lib/app/sampleDate.dart
+++ lib/app/sampleDate.dart
@@ -1,1 +1,26 @@
 // import '../models/view.dart';
+
+
+populateAllWithDummyData() {
+
+}
+
+populateConsumablesWithDummyData() {
+
+}
+
+populateProductsWithDummyData() {
+
+}
+
+populateGroceriesWithDummyData() {
+
+}
+
+populatePostsWithDummyData() {
+
+}
+
+populateSavedPostsWithDummyData() {
+  
+}
