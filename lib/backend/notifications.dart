83516dd558f310a880fd46ff1e15c2af16296c66
david mulligan <s00155368@mail.itsligo.ie>
2019-03-19 17:14:54 +0000

    added fix to androidX build issues

--- lib/backend/notifications.dart
+++ lib/backend/notifications.dart
@@ -1,2 +1,15 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
+FlutterLocalNotificationsPlugin notificationsPlugin = FlutterLocalNotificationsPlugin();
+
+notificationsInit() {
+  var initializationSettingsAndroid = AndroidInitializationSettings('app_icon');
+  // var initializationSettingsIOS = IOSInitializationSettings(onDidReceiveLocalNotification: );
+  // var initializationSettings = InitializationSettings(initializationSettingsAndroid, initializationSettingsIOS);
+
+  // notificationsPlugin.initialize(initializationSettings, onSelectNotification: onSelectionNotification);
+}
+
+Future onSelectionNotification(String payload) {
+
+}
